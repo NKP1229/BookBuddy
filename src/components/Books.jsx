1cf3f97754f06e7f0e87d1bd0b51871ff74f5174
NKP1229 <113551880+NKP1229@users.noreply.github.com>
2025-04-11 22:17:09 -0400

    Books display on home page in a grid-like/table orientation

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -5,5 +5,30 @@ import { Link } from "react-router-dom";
 import React from "react";
 
 export default function Books(){
-
+    const [addBook] = useAddBookMutation();
+    const { status, isLoading, data: bookList } = useGetBooksQuery();
+    return(
+        <article>
+            <h2>Available books...</h2>
+            <ul className="myBooks">
+                {isLoading && <li>Loading books...</li>}
+                {bookList?.map((p) => (
+                <li key={p.id}>
+                    <Link to={`/books/${p.id}`}>
+                        <h4>
+                            {p.title} #{p.id}
+                        </h4>
+                    </Link>
+                    <figure>
+                        <img src={p.coverimage} alt={p.title} />
+                    </figure>
+                    {/* <button onClick={() => navigate(viewBook)}>
+                    See details
+                    </button> */}
+                </li>
+                ))}
+            </ul>
+            
+        </article>
+    )
 }
